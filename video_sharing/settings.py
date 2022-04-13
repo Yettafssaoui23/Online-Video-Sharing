ba324c6e22775a49e0a9fe674807c8e54d265c2f
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-04-13 02:38:45 +0000

    updating static files

--- video_sharing/settings.py
+++ video_sharing/settings.py
@@ -48,8 +48,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main/static')
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_URL = "/media/"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
